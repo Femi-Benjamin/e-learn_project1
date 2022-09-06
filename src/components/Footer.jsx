705b31828377b3f7c99cf1a97e7ee7c4563464d4
Femi-Benjamin <benjaminolufemi16@gmail.com>
2022-09-06 11:28:02 -0700

    v1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,20 +3,20 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className='bg-[#0D0C42] h-[269px] mt-[119px]'>
-    <div className='grid grid-cols-3 text-white p-[50px]'>
+    <div className='grid grid-cols-3 text-white p-[50px] text-center'>
           <div>
-              <h1 className='p-7'>
+              <h1 className='p-7 '>
               Ⓒ Copyright 2022 by Ritman University
               </h1>
           </div>
 
           <div>
-                <h1 className='font-bold p-7'>
+                <h1 className='font-bold p-7 text-[20px]'>
                   Contact Us
                 </h1>
 
-                <h1>
-                  Tel: +234 808 9835170 <br />
+                <h1 className='font-semibold'>
+                  Tel: +234 808 9835170 
                       +234 803 7044797
                 </h1>
                 <p>
@@ -25,11 +25,11 @@ export default function Footer() {
           </div>
 
           <div>
-              <h1 className='font-bold p-7'>
+              <h1 className='font-bold p-7 text-[20px]'>
                 Campus Address
               </h1>
               <p>
-                104b Umahia road, Ikot Ekpene, <br />
+                104b Umahia road, Ikot Ekpene, 
                 Akwa Ibom State. Nigeria.
               </p>
           </div>
